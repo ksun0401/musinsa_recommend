3ba0d01aba7d9fb12111a4e34cdcadbaa35cab02
Coby <70461025+ksun0401@users.noreply.github.com>
2022-08-18 21:17:58 +0900

    ~ing

--- musinsa.py
+++ musinsa.py
@@ -3,85 +3,126 @@ import time
 from datetime import timedelta
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import urllib.request
 
-category = {"반소매 티셔츠":"001001", "셔츠/블라우스":"001002", "피케/카라티 셔츠":"001003",
-            "후드 티셔츠":"001004", "맨투맨/스웨트 셔츠":"001005", "니트/스웨터":'001006',
-            "기타 상의":"001008", "긴소매 티셔츠":"001010", "후드 집업":"002022", "블루종/MA-1":"002001",
-            "레더/라이더스 재킷":"002002", "무스탕/퍼":"002025", "트러커재킷":"002017",
-            "슈트/블레이저 재킷":"002003", "가디건":"002020", "플리스/뽀글이":"002023",
-            "트레이닝 재킷":"002018", "바시티 재킷":"002004", "환절기코트":"002008", "싱글 코트":"002007",
-            "더블 코트":"002024", "롱패딩":"002013", "숏패딩":"002012", "나일론/코치 재킷":"002006",
+category = {"반팔티":"001001", "셔츠,블라우스":"001002", "카라티":"001003",
+            "후드티":"001004", "맨투맨":"001005", "니트 스웨터":'001006',
+            "기타 상의":"001008", "긴팔티":"001010", "후드 집업":"002022", "블루종":"002001",
+            "레더 재킷":"002002", "무스탕":"002025", "트러커 재킷":"002017",
+            "블레이저 재킷":"002003", "가디건":"002020", "플리스":"002023",
+            "트레이닝 재킷":"002018", "바시티 재킷":"002004", "환절기 코트":"002008", "싱글 코트":"002007",
+            "더블 코트":"002024", "롱패딩":"002013", "숏패딩":"002012", "코치(나일론) 재킷":"002006",
             "데님 팬츠":"003002", "숏팬츠":"003009", "코튼 팬츠":"003007", "레깅스":"003005",
-            "슬랙스":"003008", "트레이닝/조거 팬츠":"003004", "기타 바지":"003006"}            
+            "슬랙스":"003008", "트레이닝 팬츠":"003004", "기타 바지":"003006"}            
 
 class musinsa_crw:
-    def __init__(self, category, URL, clothes, img_len, driver):
+    def __init__(self, category, URL, clothes, page_len, driver):
         self.category = category
         self.URL = URL
         self.clothes = clothes
-        self.img_len = img_len
+        self.page_len = page_len
         self.driver = driver
-         
-    def img_crw(self, page):
+
+    # 상품 진열 페이지에서 정보 페이지의 새 탭을 열어 원본 이미지 다운로드
+    def crawling(self, page):
         i = 1
         while True:
             try:
+                # 상품 정보 페이지 열기
                 target = self.driver.find_element_by_css_selector(f'#searchList > li:nth-child({i}) > div.li_inner > div.list_img > a')
                 target.send_keys(Keys.CONTROL+'\n')
-                self.driver.switch_to.window(driver.window_handles[1])
+                
+                # 정보 페이지로 이동
+                self.driver.switch_to.window(driver.window_handles[1])  
+
+                # 원본 이미지 다운로드
                 elements = self.driver.find_element_by_xpath('//*[@id="bigimg"]').get_attribute('src')
                 urllib.request.urlretrieve(elements, os.path.join("musinsa_img", self.clothes, self.clothes+"_"+str(page)+"_"+str(i)+ ".jpg")) 
+
+                # 정보 페이지 닫고 진열 페이지로 이동
                 self.driver.close()
                 self.driver.switch_to.window(driver.window_handles[0])
                 i += 1
             except:
                 print(f'--{page} 페이지 완료--')
                 break
-        
+
+    # 진열 페이지 넘기기
     def loop_page(self, pages):
         for page in range(1, pages + 1):
-            page_number = page % 10 + 2
-            try:
-                if page_number != 2:
-                    self.driver.find_element_by_css_selector(f'#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child({page_number})').click()
-                    self.img_crw(page)
-                else:
-                    self.driver.find_element_by_css_selector('#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child(12)').click()
-                    self.img_crw(page)
-                    self.driver.find_element_by_css_selector('#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child(13)').click()
-            except:
-                print("-------크롤링 종료--------")
-                break
+            page_number = page % 10 + 2  
 
-    def pass_page(self):   
+            # 팝업 생성 시 제거 후 진행
+            time.sleep(3)
+            try:
+                pop_up = self.driver.find_element_by_css_selector("body > div.ab-iam-root.v3.ab-animate-in.ab-animate-out.ab-effect-modal.ab-show > div.ab-in-app-message.ab-background.ab-modal-interactions.ab-modal.ab-centered > button > svg")
+                pop_up.click()
+                print("팝업 제거")
+
+            except NoSuchElementException:
+                print("팝업 없음")
+
+            # 1부터 9까지 이동 
+            if page_number != 2:
+                self.driver.find_element_by_css_selector(f'#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child({page_number})').click()
+                self.crawling(page)
+            # 마지막 10페이지 이후 다음 페이지로 이동
+            else:
+                self.driver.find_element_by_css_selector('#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child(12)').click() # 10 페이지
+                self.crawling(page)
+                self.driver.find_element_by_css_selector('#goods_list > div.boxed-list-wrapper > div.sorter-box.box > div > div > a:nth-child(13)').click() # 다음 페이지
+
+        print("크롤링 종료")    
+        self.driver.close()
+
+
+
+    def musinsa_crw(self):   
         self.driver.implicitly_wait(3)
         self.driver.get(self.URL + self.category[self.clothes])
         self.clothes = self.clothes.replace(' ', '_')
         os.makedirs("musinsa_img/" + self.clothes, exist_ok = True)
 
+        # 총 페이지 수와 크롤링 할 페이지 수 비교 
         max_number = self.driver.find_element_by_xpath('//*[@id="goods_list"]/div[2]/div[2]/span[8]/span[1]')
         max_page = int(max_number.text)  
-        pages = int(self.img_len/90) + 1
         
-        if max_page <= pages:
-            print(f"이미지 수가 적어 {max_page} 페이지까지 크롤링 합니다.")
+        if max_page <= self.page_len:
+            print(f"페이지 수가 적어 {max_page} 페이지까지 크롤링 합니다.")
             self.loop_page(max_page)
         else:
-            print(f"{pages} 페이지까지 크롤링 합니다.")
-            self.loop_page(pages)
+            print(f"{self.page_len} 페이지까지 크롤링 합니다.")
+            self.loop_page(self.page_len)
 
 
-clothes = input("검색할 옷: ")
+print('''
+-----------------------카테고리------------------------------
+티셔츠: 반팔티, 긴팔티, 카라티, 후드티, 니트 스웨터, 맨투맨
 
-img_len = int(input("다운로드 이미지 수: "))
+재킷: 후드 집업, 블루종, 레더 재킷, 무스탕, 트러커 재킷, 
+         트레이닝 재킷, 바시티 재킷, 블레이저 재킷,
+         가디건, 플리스, 코치(나일론) 재킷
+         
+코트: 환절기 코트, 싱글 코트, 더블 코트
+
+패딩: 롱패딩, 숏패딩
+
+하의: 데님 팬츠, 숏팬츠, 코튼 팬츠, 레깅스,
+      슬랙스, 트레이닝 팬츠
+
+기타: 기타 상의, 기타 바지
+-------------------------------------------------------------
+''')
+
+clothes = input("가져올 의류: ")
+
+page_len = int(input("페이지 수 (한 페이지에 90개): "))
 URL = 'https://www.musinsa.com/category/'
 
-# options = webdriver.ChromeOptions()
-# options.add_experimental_option("excludeSwitches", ["enable-logging"])
-# browser = webdriver.Chrome(options=options)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome('C:\\Users\\user\\workspace\\chromedriver\\chromedriver.exe', chrome_options = options)
 
-driver = webdriver.Chrome('C:\\Users\\user\\workspace\\chromedriver\\chromedriver.exe')
-
-musinsa = musinsa_crw(category, URL, clothes, img_len, driver)
-musinsa.pass_page()
+musinsa = musinsa_crw(category, URL, clothes, page_len, driver)
+musinsa.musinsa_crw()
